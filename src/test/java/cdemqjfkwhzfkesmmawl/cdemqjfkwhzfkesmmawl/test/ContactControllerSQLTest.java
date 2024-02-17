8465abc8c0263aa8cab3a623f177c37bf923479d
Zhalgasov_Beksultan <zalgasovbeksultan1@gmail.com>
2024-02-17 19:49:35 +0600

    add Mockite and write test

--- src/test/java/cdemqjfkwhzfkesmmawl/cdemqjfkwhzfkesmmawl/test/ContactControllerSQLTest.java
+++ src/test/java/cdemqjfkwhzfkesmmawl/cdemqjfkwhzfkesmmawl/test/ContactControllerSQLTest.java
@@ -1,20 +1,55 @@
 package cdemqjfkwhzfkesmmawl.cdemqjfkwhzfkesmmawl.test;
 
-
-import cdemqjfkwhzfkesmmawl.cdemqjfkwhzfkesmmawl.repositories.ContactSQLRepository;
+import cdemqjfkwhzfkesmmawl.cdemqjfkwhzfkesmmawl.dto.ContactSQLDto;
+import cdemqjfkwhzfkesmmawl.cdemqjfkwhzfkesmmawl.modal.ContactSQL;
 import cdemqjfkwhzfkesmmawl.cdemqjfkwhzfkesmmawl.services.impls.ContactSQLServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 @SpringBootTest
 @Transactional
 public class ContactControllerSQLTest {
-    @Autowired
-    private ContactSQLRepository contactSQLRepository;
-    @Autowired
+
+    @Mock
     private ContactSQLServiceImpl contactSQLService;
 
+    @BeforeMethod
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
+    public void testGetAllContacts() {
+        ContactSQLDto contact1 = createContact("John Doe", "12.12.2001", "1234567890", "0987654321");
+        ContactSQLDto contact2 = createContact("Jane Smith", "01.01.1990", "9876543210", null);
+
+        when(contactSQLService.getAllContacts()).thenReturn(Arrays.asList(contact1, contact2));
+
+        List<ContactSQLDto> allContacts = contactSQLService.getAllContacts();
+
+        assertNotNull(allContacts);
+        assertEquals(2, allContacts.size());
+        assertEquals(contact1.getName(), allContacts.get(0).getName());
+        assertEquals(contact2.getName(), allContacts.get(1).getName());
+    }
+
+    private ContactSQLDto createContact(String name, String dateOfBirth, String firstPhoneNumber, String secondPhoneNumber) {
+        ContactSQL contact = new ContactSQL();
+        contact.setName(name);
+        contact.setDate_Of_Birth(dateOfBirth);
+        contact.setFirstPhoneNumber(firstPhoneNumber);
+        contact.setSecondPhoneNumber(secondPhoneNumber);
+        return contactSQLService.createContact(contact);
+    }
 }
